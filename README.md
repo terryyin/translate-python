1d5bb8b3e2034be0a4178c01f217eb4b3eeba64b
terryyin <terry@odd-e.com>
2013-05-08 12:59:38 +0800

    register to PIP (as translate)

--- README.md
+++ README.md
@@ -4,21 +4,33 @@ google-translate-python
 Google translate as Python module &amp; command line tool. No key, no authentication whatsoever.
 
 ## Installation
+
+<pre>
+pip install translate
+</pre> 
+
+Or, you can download the source and
+
 <pre>
 python setup.py install
 </pre> 
+
 Add sudo in the beginning if you met problem.
 
 ## Command-Line Usage
+
 In your command-line:
+
 <pre>
 translate "This is a pen."
 </pre>
 
 ## Use As A Python Module
+
 <pre>
 from translate import Translator
 translator= Translator(to_lang="zh")
 translation = translator.translate("This is a pen.")
 </pre>
+
 The result is in translation, and it's usually a unicode string.
--- setup.py
+++ setup.py
@@ -2,15 +2,35 @@
 # encoding: utf-8
 '''
 '''
-
+import translate
 from distutils.core import setup
 def install():
-    setup(name = 'othello',
-          version = "0.0.1",
+    try:
+        long_description = open("README.md").read()
+    except:
+        long_description = translate.__doc__
+    setup(
+          name = 'translate',
+          version = "0.0.3",
+          description = translate.__doc__,
+          long_description = long_description,
+          url = 'https://github.com/terryyin/google-translate-python',
+          classifiers = ['Development Status :: 4 - Beta',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: Freeware',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Education',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.6',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.2',
+                     'Programming Language :: Python :: 3.3'],
           py_modules = ['translate'],
           author = 'Terry Yin',
           author_email = 'terry.yinze@gmail.com',
-          url= 'https://github.com/terryyin/translate',
           scripts=['translate']
           )
 
