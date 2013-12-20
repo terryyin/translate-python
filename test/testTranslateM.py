458c7da9ff2628ba1c20462e71ffead3d4d3ae0d
geekstay <geekstay@gmail.com>
2013-12-20 19:04:13 +0100

    Test files

---
+++ test/testTranslateM.py
@@ -0,0 +1,35 @@
+import unittest
+from translate import Translator
+from subprocess import check_output
+from os import system, remove
+
+def readFile(s):
+    return [i.strip() for i in open(s)]
+
+class TranslateMTest(unittest.TestCase):
+            
+    def setUp(self):
+        self.f_in = "TranslateMTestIn.tFile"
+        self.f_out = "TranslateMTestOut.tFile"
+        self.f_tmp = "TranslateMTestTmp.tFile"
+        
+    def test_commandline_input_file_output_std(self):
+        result = check_output(["translate", "-i", self.f_in, " "])
+        result = result.split("\n")
+        result.remove("")
+        self.assertEqual(result, readFile(self.f_out))
+    
+    def test_commandline_input_file_output_file(self):
+        cmd = ["translate", "-i", self.f_in, "-o", self.f_tmp, "h"]
+        system(" ".join(cmd))
+        self.assertEqual(readFile(self.f_tmp), readFile(self.f_out))
+        remove(self.f_tmp)
+
+    def test_commandline_input_std_output_file(self):
+        cmd = ["translate", "-o", self.f_tmp, "hello"]
+        system(" ".join(cmd))
+        self.assertEqual(['bonjour'], readFile(self.f_tmp))
+        remove(self.f_tmp)
+
+
+unittest.main()
