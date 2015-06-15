09e2a7330ec49d35dc8966c71a2903690547b4e8
Terry Yin <terry.yinzhe@gmail.com>
2015-06-15 15:38:00 +0800

    Revert "Fix for instances where Boolean is returned as translation. #15"

--- translate.py
+++ translate.py
@@ -16,6 +16,7 @@
 This is a simple, yet powerful command line translator with google translate
 behind it. You can also use it as a Python module in your code.
 '''
+import re
 import json
 from textwrap import wrap
 try:
@@ -38,30 +39,18 @@ class Translator:
 
     def _get_translation_from_google(self, source):
         json5 = self._get_json5_from_google(source)
-        data = json.loads(json5)
-        translation = data['responseData']['translatedText']
-        if not isinstance(translation, bool):
-            return translation
-        else:
-            matches = data['matches']
-            for match in matches:
-                if not isinstance(match['translation'], bool):
-                    next_best_match = match['translation']
-                    break
-            return next_best_match
+        return json.loads(json5)['responseData']['translatedText']
 
     def _get_json5_from_google(self, source):
         escaped_source = quote(source, '')
-        headers = {'User-Agent':
-                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19\
-                   (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19'}
-        api_url = "http://mymemory.translated.net/api/get?q=%s&langpair=%s|%s"
-        req = request.Request(url=api_url % (escaped_source, self.from_lang, self.to_lang),
-                              headers=headers)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19'}
+        req = request.Request(
+             url="http://mymemory.translated.net/api/get?q=%s&langpair=%s|%s" % (escaped_source, self.from_lang, self.to_lang)
+                 , headers = headers)
 
-        # url="http://translate.google.com/translate_a/t?clien#t=p&ie=UTF-8&oe=UTF-8"
-        # +"&sl=%s&tl=%s&text=%s" % (self.from_lang, self.to_lang, escaped_source)
-        # , headers = headers)
+             #url="http://translate.google.com/translate_a/t?clien#t=p&ie=UTF-8&oe=UTF-8"
+                 #+"&sl=%s&tl=%s&text=%s" % (self.from_lang, self.to_lang, escaped_source)
+                 #, headers = headers)
         r = request.urlopen(req)
         return r.read().decode('utf-8')
 
@@ -71,21 +60,21 @@ def main(defvals=None):
     import locale
 
     if defvals is None:
-        defvals = {'f': 'en', 't': 'zh'}
+       defvals = {'f':'en', 't':'zh'} 
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('texts', metavar='text', nargs='+',
-                        help='a string to translate(use "" when it\'s a sentence)')
+                   help='a string to translate(use "" when it\'s a sentence)')
     parser.add_argument('-t', '--to', dest='to_lang', type=str, default=defvals['t'],
-                        help='To language (e.g. zh, zh-TW, en, ja, ko). Default is '+defvals['t']+'.')
+                   help='To language (e.g. zh, zh-TW, en, ja, ko). Default is '+defvals['t']+'.')
     parser.add_argument('-f', '--from', dest='from_lang', type=str, default=defvals['f'],
-                        help='From language (e.g. zh, zh-TW, en, ja, ko). Default is '+defvals['f']+'.')
+                   help='From language (e.g. zh, zh-TW, en, ja, ko). Default is '+defvals['f']+'.')
     args = parser.parse_args()
-    translator = Translator(from_lang=args.from_lang, to_lang=args.to_lang)
+    translator= Translator(from_lang=args.from_lang, to_lang=args.to_lang)
     for text in args.texts:
         translation = translator.translate(text)
         if sys.version_info.major == 2:
-            translation = translation.encode(locale.getpreferredencoding())
+            translation =translation.encode(locale.getpreferredencoding())
         sys.stdout.write(translation)
         sys.stdout.write("\n")
 
