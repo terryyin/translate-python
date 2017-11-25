ec1bcd63c38e90d76e1db5440cb8687babcb97e0
Rafael Henter <henter.r@gmail.com>
2017-11-24 22:15:47 -0200

    Remove dead code

--- translate/main.py
+++ translate/main.py
@@ -38,23 +38,15 @@ def get_config_info():
 @click.command()
 @click.option(
     'from_lang', '--from', '-f',
-    # default=TRANSLATION_FROM_DEFAULT,
     help='Language to be translated.'
 )
 @click.option(
     'to_lang', '--to', '-t',
-    # default=TRANSLATION_TO_DEFAULT,
     help='Language to be translated.'
 )
 @click.argument('text', nargs=-1, type=click.STRING)
 def main(from_lang, to_lang, text):
     config_info = get_config_info()
-    # if config_info:
-    #     not_default_from_condition = from_lang != TRANSLATION_FROM_DEFAULT
-    #     from_lang = from_lang if not_default_from_condition else config_info['from_lang']
-
-    #     not_default_to_condition = to_lang != TRANSLATION_FROM_DEFAULT
-    #     to_lang = to_lang if not_default_to_condition else config_info['to_lang']
     from_lang = from_lang or config_info['from_lang']
     to_lang = to_lang or config_info['to_lang']
 
