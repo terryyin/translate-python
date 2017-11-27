fab9cf34ee5be9bd8e051fd372c69b4ac54f1638
Rafael Henter <henter.r@gmail.com>
2017-11-27 20:43:14 -0200

    Refactor provider to make simple add new providers.

---
+++ translate/providers/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from abc import ABCMeta, abstractmethod
+
+
+class BaseProvider:
+    __metaclass__ = ABCMeta
+
+    base_url = ''
+
+    def __init__(self, to_lang, from_lang='en', secret_access_key=None, **kwargs):
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebit/535.19'
+                                      '(KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19'}
+        self.from_lang = from_lang
+        self.to_lang = to_lang
+        self.secret_access_key = secret_access_key
+
+    @abstractmethod
+    def get_translation(self, params):
+        return NotImplemented('Please Implement this method')
--- translate/providers/mymemory_translated.py
+++ translate/providers/mymemory_translated.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
-from abc import ABCMeta, abstractmethod
-
 import requests
 
-
-class BaseProvider:
-    __metaclass__ = ABCMeta
-
-    base_url = ''
-
-    def __init__(self, to_lang, headers, from_lang='en', secret_access_key=None, **kwargs):
-        self.from_lang = from_lang
-        self.to_lang = to_lang
-        self.headers = headers
-        self.secret_access_key = secret_access_key
-
-    @abstractmethod
-    def get_translation(self, params):
-        return NotImplemented('Please Implement this method')
+from .base import BaseProvider
 
 
 class MyMemoryProvider(BaseProvider):
     '''
-    @MyMemoryProvider: http://mymemory.translated.net
-    Usage: Use a valid email instead of the default.
-    With a valid email you get 10 times more words/day to translate.
+    @MyMemoryProvider: This is a integration with Translated MyMemory API.
+    Follow Informations:
+      Website: https://mymemory.translated.net/
+      Documentation: https://mymemory.translated.net/doc/spec.php
+
+    Usage Tips: Use a valid email instead of the default.
+        With a valid email you get 10 times more words/day to translate.
     For further infomations checkout:
     http://mymemory.translated.net/doc/usagelimits.php
-    http://mymemory.translated.net/doc/spec.php  Tips from: @Bachstelze
+                                                    Tips from: @Bachstelze
     '''
     base_url = 'http://api.mymemory.translated.net/get'
 
@@ -45,7 +32,7 @@ class MyMemoryProvider(BaseProvider):
     def _make_request(self, text):
         params = {'q': text, 'langpair': self.languages}
         if self.email:
-            params['email'] = self.email
+            params['de'] = self.email
 
         response = requests.get(self.base_url, params=params, headers=self.headers)
         return response.json()
